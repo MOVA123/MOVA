40f01b2957761d44d21b4dab53a7d85c3affef88
MOVA123 <gashoycoccaont@outlook.com>
2025-05-11 23:33:43 +0800

    Add files via upload

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,4 +1,13 @@
 import "../globals.css"
+import type { Metadata } from "next"
+
+export const metadata: Metadata={
+  title: 'Login | Mova',
+  icons: {
+    icon:'/assets/icons/logo.ico'
+  }
+}
+
 export default function Layout({
   children,
 }: Readonly<{
