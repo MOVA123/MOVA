4cf1fd12a9f166ab386c110f9f5d31b0328084d7
MOVA123 <gashoycoccaont@outlook.com>
2025-05-11 23:34:49 +0800

    Add files via upload

--- src/app/(common)/page.tsx
+++ src/app/(common)/page.tsx
@@ -1,6 +1,15 @@
 import Link from "next/link";
 import VideoSwiper from "./components/videoSwiper";
 import Image from "next/image";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Mova',
+  icons: {
+    icon: '/assets/icons/logo.ico'
+  }
+}
+
 export default function Page() {
   return (
     <main>
